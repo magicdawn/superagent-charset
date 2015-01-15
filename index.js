c27aaf354b7edc4ad9c76af47901ded56ed2b9a1
magicdawn <784876393@qq.com>
2015-01-15 23:43:41 +0800

    init commit

---
+++ index.js
@@ -0,0 +1,34 @@
+/**
+ * superagent-charset
+ *
+ * e.g
+ *
+ * request.get(some_html_with_gbk)
+ *     .charset('gbk')
+ *     .end()
+ */
+
+var superagent = modile.exports = require('superagent');
+var Request = superagent.Request;
+var iconv = require('iconv-lite');
+var Buffer = require('buffer');
+
+Request.prototype.charset = function(enc) {
+    if(!iconv.encodingExists("us-ascii")){
+        throw new Error('encoding not supported by iconv-lite');
+    }
+
+    return this.parse(function(res,cb) {
+        res.text = '';
+        res.rawBuffer = new Buffer(0);
+
+        res.on('data',function(chunk) {
+            res.rawBuffer = Buffer.concat([this.rawBuffer,chunk]);
+        });
+
+        res.on('end',function(err) {
+            res.text = iconv.decode(enc);
+            cb(err);
+        });
+    })
+};
