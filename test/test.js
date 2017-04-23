351beb5ccb5c6255b469e4050d20d66742445866
magicdawn <magicdawn@qq.com>
2017-04-23 15:13:51 +0800

    test: use async-await for testing

--- test/test.js
+++ test/test.js
@@ -3,16 +3,14 @@
 const should = require('should')
 const request = require('superagent')
 const charset = require('../')
-charset(request) // install charset;
+charset(request) // install charset
 
 describe('Basic Test', function() {
-  it('it works', function(done) {
-    request.get('http://www.sohu.com/')
+  it('it works', async function() {
+    const res = await request
+      .get('http://www.sohu.com/')
       .charset('gbk')
-      .end((err, res) => {
-        res.text.should.match(/搜狐/)
-        done(err)
-      })
+    res.text.should.match(/搜狐/)
   })
 
   it('bad charset', function() {
@@ -23,30 +21,24 @@ describe('Basic Test', function() {
     }).should.throw('encoding not supported by iconv-lite')
   })
 
-  it('automatic detection by headers', function(done) {
-    request.get('http://www.qq.com/')
+  it('automatic detection by headers', async function() {
+    const res = await request
+      .get('http://www.qq.com/')
       .charset() // automatic detection
-      .end((err, res) => {
-        res.text.should.match(/腾讯/)
-        done(err)
-      })
+    res.text.should.match(/腾讯/)
   })
 
-  it('automatic detection by meta', function(done) {
-    request.get('http://acm.hdu.edu.cn/showproblem.php?pid=2000')
+  it('automatic detection by meta', async function() {
+    const res = await request
+      .get('http://acm.hdu.edu.cn/showproblem.php?pid=2000')
       .charset() // automatic detection
-      .end((err, res) => {
-        res.text.should.match(/ASCII码排序/)
-        done(err)
-      })
+    res.text.should.match(/ASCII码排序/)
   })
 
-  it('automatic detection by default utf-8', function(done) {
-    request.get('http://files.cnblogs.com/files/52cik/cnblogs.css')
+  it('automatic detection by default utf-8', async function() {
+    const res = await request
+      .get('http://files.cnblogs.com/files/52cik/cnblogs.css')
       .charset() // automatic detection
-      .end((err, res) => {
-        res.text.should.match(/布局修改/)
-        done(err)
-      })
+    res.text.should.match(/布局修改/)
   })
 })
