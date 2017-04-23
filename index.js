cddf720d2509abb83bf5cc3197ee60b3f751f789
magicdawn <magicdawn@qq.com>
2017-04-23 15:06:57 +0800

    eslint: fix code

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-'use strict';
+'use strict'
 
 /**
  * module dependencies
  */
 
-const iconv = require('iconv-lite');
+const iconv = require('iconv-lite')
 
 /**
  * util
@@ -13,16 +13,16 @@ const iconv = require('iconv-lite');
 const checkEncoding = enc => {
   // check iconv supported encoding
   if (enc && !iconv.encodingExists(enc)) {
-    return new Error('encoding not supported by iconv-lite');
+    return new Error('encoding not supported by iconv-lite')
   }
-};
+}
 
 /**
  * install the charset()
  */
 
 module.exports = function install(superagent) {
-  const Request = superagent.Request;
+  const Request = superagent.Request
 
   /**
    * add `charset` to request
@@ -32,55 +32,55 @@ module.exports = function install(superagent) {
 
   Request.prototype.charset = function(enc) {
     {
-      let err;
-      if ((err = checkEncoding(enc))) throw err;
+      let err
+      if ((err = checkEncoding(enc))) throw err
     }
 
     // set the parser
     this._parser = function(res, cb) { // res not instanceof http.IncomingMessage
-      const buffer = [];
+      const buffer = []
 
       res.on('data', function(chunk) {
-        buffer.push(chunk);
-      });
+        buffer.push(chunk)
+      })
 
       res.on('end', function() {
-        let text, err;
+        let text, err
 
         // detect if encoding if not specified
         if (!enc) {
           if (res.headers['content-type']) {
             // Extracted from headers
-            enc = (res.headers['content-type'].match(/charset=(.+)/) || []).pop();
+            enc = (res.headers['content-type'].match(/charset=(.+)/) || []).pop()
           }
 
           if (!enc) {
             // Extracted from <meta charset="gb2312"> or <meta http-equiv=Content-Type content="text/html;charset=gb2312">
-            enc = (buffer.toString().match(/<meta.+?charset=['"]?([^"']+)/i) || []).pop();
+            enc = (buffer.toString().match(/<meta.+?charset=['"]?([^"']+)/i) || []).pop()
           }
 
           // check
-          if ((err = checkEncoding(enc))) return cb(err);
+          if ((err = checkEncoding(enc))) return cb(err)
 
           if (!enc) {
             // Default utf8
-            enc = 'utf-8';
+            enc = 'utf-8'
           }
         }
 
         try {
-          text = iconv.decode(Buffer.concat(buffer), enc);
+          text = iconv.decode(Buffer.concat(buffer), enc)
         } catch (e) {
-          err = e;
+          err = e
         } finally {
-          res.text = text;
-          cb(err);
+          res.text = text
+          cb(err)
         }
-      });
-    };
+      })
+    }
 
-    return this;
-  };
+    return this
+  }
 
-  return superagent;
-};
+  return superagent
+}
--- test/test.js
+++ test/test.js
@@ -1,52 +1,52 @@
-'use strict';
+'use strict'
 
-const should = require('should');
-const request = require('superagent');
-const charset = require('../');
-charset(request); // install charset;
+const should = require('should')
+const request = require('superagent')
+const charset = require('../')
+charset(request) // install charset;
 
 describe('Basic Test', function() {
   it('it works', function(done) {
     request.get('http://www.sohu.com/')
       .charset('gbk')
       .end((err, res) => {
-        res.text.should.match(/搜狐/);
-        done(err);
-      });
-  });
+        res.text.should.match(/搜狐/)
+        done(err)
+      })
+  })
 
   it('bad charset', function() {
     (function() {
       request.get('https://www.baidu.com/')
         .charset('bad-charset')
-        .end();
-    }).should.throw('encoding not supported by iconv-lite');
-  });
+        .end()
+    }).should.throw('encoding not supported by iconv-lite')
+  })
 
   it('automatic detection by headers', function(done) {
     request.get('http://www.qq.com/')
       .charset() // automatic detection
       .end((err, res) => {
-        res.text.should.match(/腾讯/);
-        done(err);
-      });
-  });
+        res.text.should.match(/腾讯/)
+        done(err)
+      })
+  })
 
   it('automatic detection by meta', function(done) {
     request.get('http://acm.hdu.edu.cn/showproblem.php?pid=2000')
       .charset() // automatic detection
       .end((err, res) => {
-        res.text.should.match(/ASCII码排序/);
-        done(err);
-      });
-  });
+        res.text.should.match(/ASCII码排序/)
+        done(err)
+      })
+  })
 
   it('automatic detection by default utf-8', function(done) {
     request.get('http://files.cnblogs.com/files/52cik/cnblogs.css')
       .charset() // automatic detection
       .end((err, res) => {
-        res.text.should.match(/布局修改/);
-        done(err);
-      });
-  });
-});
+        res.text.should.match(/布局修改/)
+        done(err)
+      })
+  })
+})
